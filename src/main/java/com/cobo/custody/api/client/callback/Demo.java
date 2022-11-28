3d208ac3cb006b2e17713f817ddd317eb1997766
haofengjiang <115331712+haofengjiang@users.noreply.github.com>
2022-11-28 17:49:02 +0800

    release new mpc api

--- src/main/java/com/cobo/custody/api/client/callback/Demo.java
+++ src/main/java/com/cobo/custody/api/client/callback/Demo.java
@@ -27,12 +27,9 @@ public class Demo {
     };
 
     public static void main(String[] args) {
-        String timestamp = "1669376201198";
-        String signature = "304402202485670f2579c3cbbbed9817f9263cb2210d57e322540aa87b9ba1c57703c66602200c0f09f182c33087c43af3096ff0f55b4cedd746287e73bd197369b89f1408ff";
-        String body = "{\"id\": \"20221125163239000340987000006912\", \"coin\": \"TRON_USDT\", \"display_code\": \"USDT\", \"description\": \"Tether\", \"decimal\": 6, \"address\": \"TAc4jeURBoCjztN78KcJvezEGihKod4zvW\", \"source_address\": \"TSaRZDiBPD8Rd5vrvX8a4zgunHczM9mj8S\", \"side\": \"deposit\", \"amount\": \"10000000\", \"abs_amount\": \"10\", \"txid\": \"6a63e49b60a0b74bd146e90c8703fe3745bad78a551048e5b7660e0bcec9150e\", \"vout_n\": 0, \"request_id\": null, \"status\": \"success\", \"abs_cobo_fee\": \"0\", \"created_time\": 1669365257204, \"last_time\": 1669365257204, \"confirmed_num\": 33, \"tx_detail\": {\"txid\": \"6a63e49b60a0b74bd146e90c8703fe3745bad78a551048e5b7660e0bcec9150e\", \"blocknum\": 46259387, \"blockhash\": \"0000000002c1dcbbbd3ffa9a999c340d7e4d6814ccef3fcb4f996e38afe7bc8f\", \"hexstr\": \"\"}, \"source_address_detail\": \"TSaRZDiBPD8Rd5vrvX8a4zgunHczM9mj8S\", \"confirming_threshold\": 33, \"type\": \"external\"}";
-        String content = body + "|" + timestamp;
-        boolean verifyResult = LocalSigner.verifyEcdsaSignature(content, signature, coboPubKey);
-        System.out.println("verifyResult:\t"  + verifyResult);
+        Blade.of().listen(9000)
+                .get("/", ctx -> ctx.text("ok!"))
+                .post("/custody_callback", custodyCallback).start();
     }
 
     public static boolean customCheck(RouteContext ctx) {
