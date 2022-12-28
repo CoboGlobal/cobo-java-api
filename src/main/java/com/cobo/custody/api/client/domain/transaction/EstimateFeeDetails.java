1eaf559704e2f8dd443aed5e1bdf68a9d7f2e507
haofengjiang <haofeng.jiang@cobo.com>
2022-12-28 15:12:44 +0800

    update mpc api sdk

--- src/main/java/com/cobo/custody/api/client/domain/transaction/EstimateFeeDetails.java
+++ src/main/java/com/cobo/custody/api/client/domain/transaction/EstimateFeeDetails.java
@@ -1,9 +1,18 @@
 package com.cobo.custody.api.client.domain.transaction;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class EstimateFeeDetails {
     private EstimateFeeDetail slow;
     private EstimateFeeDetail average;
     private EstimateFeeDetail fast;
+
+    @JsonProperty(value = "fee_coin")
+    private String feeCoin;
+
+    @JsonProperty(value = "fee_decimal")
+    private Integer feeDecimal;
+
 
     public EstimateFeeDetail getSlow() {
         return slow;
@@ -29,12 +38,30 @@ public class EstimateFeeDetails {
         this.fast = fast;
     }
 
+    public String getFeeCoin() {
+        return feeCoin;
+    }
+
+    public void setFeeCoin(String feeCoin) {
+        this.feeCoin = feeCoin;
+    }
+
+    public Integer getFeeDecimal() {
+        return feeDecimal;
+    }
+
+    public void setFeeDecimal(Integer feeDecimal) {
+        this.feeDecimal = feeDecimal;
+    }
+
     @Override
     public String toString() {
         return "{" +
                 "slow='" + slow + '\'' +
                 ", average='" + average + '\'' +
                 ", fast='" + fast + '\'' +
+                ", feeCoin='" + feeCoin + '\'' +
+                ", feeDecimal='" + feeDecimal + '\'' +
                 '}';
     }
 }
