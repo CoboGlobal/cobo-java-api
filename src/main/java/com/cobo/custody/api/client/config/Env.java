540b2e76de118059ad00d5d63fa54639b5cc50f7
Huiwen Zhang <zhanghuiwen@cobo.com>
2023-08-25 11:20:33 +0800

    Update Develop Environment

--- src/main/java/com/cobo/custody/api/client/config/Env.java
+++ src/main/java/com/cobo/custody/api/client/config/Env.java
@@ -1,7 +1,7 @@
 package com.cobo.custody.api.client.config;
 
 public class Env {
-    public static Env DEVELOP = new Env("https://api.develop.cobo.com","03596da539963fb1dd29d5859e25903eb76b9f7ed2d58516e29c9f80c201ff2c1b");
+    public static Env DEVELOP = new Env("https://api.dev.cobo.com","03596da539963fb1dd29d5859e25903eb76b9f7ed2d58516e29c9f80c201ff2c1b");
     public static Env PROD = new Env("https://api.custody.cobo.com", "02c3e5bacf436fbf4da78597e791579f022a2e85073ae36c54a361ff97f2811376");
 
     public String host;
