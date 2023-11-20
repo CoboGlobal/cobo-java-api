9dae48717a78ecab753eb742e4f577cd2871a71d
Haofeng Jiang <haofeng.jiang@cobo.com>
2023-11-20 17:53:41 +0800

    update

--- src/main/java/com/cobo/custody/api/client/impl/CoboMPCPrimeBrokerService.java
+++ src/main/java/com/cobo/custody/api/client/impl/CoboMPCPrimeBrokerService.java
@@ -7,27 +7,27 @@ import retrofit2.http.*;
 
 public interface CoboMPCPrimeBrokerService {
     @FormUrlEncoded
-    @POST("/v1/custody/guards/create_binding/")
+    @POST("/v1/custody/guard/create_binding/")
     Call<ApiResponse<PrimeBrokerBinderInfo>> createBinding(@Field("user_id") String userId);
 
-    @GET("/v1/custody/guards/query_binding/")
+    @GET("/v1/custody/guard/query_binding/")
     Call<ApiResponse<PrimeBrokerUserBindInfo>> queryBinding(@Query("binder_id") String binderId);
 
-    @GET("/v1/custody/guards/query_user_auth/")
+    @GET("/v1/custody/guard/query_user_auth/")
     Call<ApiResponse<PrimeBrokerUserAuthInfo>> queryUserAuth(@Query("user_id") String userId);
 
     @FormUrlEncoded
-    @POST("/v1/custody/guards/bind_addresses/")
+    @POST("/v1/custody/guard/bind_addresses/")
     Call<ApiResponse<PrimeBrokerUserAuthInfo>> bindAddresses(@Field("user_id") String userId, @Field("addresses") String addresses);
 
     @FormUrlEncoded
-    @POST("/v1/custody/guards/change_binding/")
+    @POST("/v1/custody/guard/change_binding/")
     Call<ApiResponse<PrimeBrokerBinderInfo>> changeBinding(@Field("user_id") String userId);
 
     @FormUrlEncoded
-    @POST("/v1/custody/guards/unbind_binding/")
+    @POST("/v1/custody/guard/unbind_binding/")
     Call<ApiResponse<StatementId>> unbindBinding(@Field("user_id") String userId);
 
-    @GET("/v1/custody/guards/query_statement/")
+    @GET("/v1/custody/guard/query_statement/")
     Call<ApiResponse<Statement>> queryStatement(@Query("statement_id") String statementId);
 }
